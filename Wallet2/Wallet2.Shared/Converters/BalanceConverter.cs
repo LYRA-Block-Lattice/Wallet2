9ed80ca04908c41ce3b38c3aa13e5fbfa075c653
Wizard Young <rcvbuf@gmail.com>
2020-10-29 06:43:29 +0800

    added balance converter

---
+++ Wallet2/Wallet2.Shared/Converters/BalanceConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Windows.UI.Xaml.Data;
+
+namespace Wallet2.Shared.Converters
+{
+    public sealed class BalanceConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value is decimal balance)
+                return $"{(float)balance / 100000000}";
+
+            return "0";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter,
+            string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
