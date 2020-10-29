62585e086fb2ecc9e431bff3f31fd049c6b4e1d8
Wizard Young <rcvbuf@gmail.com>
2020-10-30 02:11:05 +0800

    send token is OK.

--- Wallet2/Wallet2.Shared/Pages/SendToken.xaml.cs
+++ Wallet2/Wallet2.Shared/Pages/SendToken.xaml.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using LyraWallet.States;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -22,14 +24,34 @@ namespace Wallet2.Shared.Pages
     /// </summary>
     public sealed partial class SendToken : Page
     {
+        List<string> tokens;
+        string selectedToken;
+        string toAddress;
+        string amountString;
+
         public SendToken()
         {
             this.InitializeComponent();
+
+            tokens = App.Store.State.wallet.GetLatestBlock()?.Balances?.Keys.ToList();
         }
 
         private void Send_Click(object sender, RoutedEventArgs e)
         {
+            // TODO: add argument check here
 
+            var moAct = new WalletSendTokenAction
+            {
+                DstAddr = toAddress,
+                Amount = decimal.Parse(amountString),
+                TokenName = selectedToken,
+                wallet = App.Store.State.wallet
+            };
+            //var mtxt = "Transfering funds...";
+
+            _ = Task.Run(() => { App.Store.Dispatch(moAct); });
+
+            Frame.Navigate(typeof(MainPage));
         }
 
         private void Scan_Click(object sender, RoutedEventArgs e)
